11cb9487e76be4142f32848a8451e4a49b3954e3
Roberto E Ferrer P <rferrer@mint.com>
2014-07-24 09:36:13 -0430

    update

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -13,7 +13,7 @@
 dir=~/dotfiles # dotfiles directory
 olddir=~/dotfiles_old # old dotfiles backup directory
 # list of files/folders to  symlink in homedir
-files="bashrc gitconfig gitignore profile tmux.conf vimrc" 
+files="bashrc gitconfig gitignore profile tmux.conf vimrc rtorrent.rc" 
 
 
 ########## Processing
